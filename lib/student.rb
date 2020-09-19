60e7e4c8565d5c8fb88e142c1a04d86f2d8f02e2
Lauren Kohn <lauren.kohn@soft-adv.com>
2020-09-19 04:38:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,8 +48,8 @@ class Student
   end 
   
   def self.new_from_db(array)
-    #new_student = Student.new 
-    #new_student.id = array[0]
+    new_student = Student.new 
+    new_student.id = array[0]
     #new_student.name = array[1]
     #new_student.grade = array[2]
     #new_student
